8653fc601ebf03573e45f714753c692b75f9e659
himachalamsri <sriramh115@gmail.com>
2024-01-10 08:09:13 +0530

    added

---
+++ src/test/java/com/gre/Company.java
@@ -0,0 +1,29 @@
+package com.gre;
+
+public class Company {
+	
+	private void test1() {
+		System.out.println("test1");
+
+	}
+
+	private void test2() {
+		System.out.println("test2");
+
+	}
+	private void test3() {
+		System.out.println("test3");
+
+	}
+	private void test4() {
+		System.out.println("test4");
+
+	}
+	
+	public static void main(String[] args) {
+		
+		Company c=new Company();
+		c.test1();
+		
+	}
+}
