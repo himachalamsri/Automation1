6a52214b0a6f6eb154835153f78441ce9180dd25
sriramhimlak <sriramhim77@gmail.com>
2024-10-08 15:45:19 +0530

    addbase

--- src/test/java/com/gre/Company.java
+++ src/test/java/com/gre/Company.java
@@ -1,29 +1,36 @@
 package com.gre;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Company {
 	
-	private void test1() {
-		System.out.println("test1");
-
-	}
-
-	private void test2() {
-		System.out.println("test2");
-
-	}
-	private void test3() {
-		System.out.println("test3");
-
-	}
-	private void test4() {
-		System.out.println("test4");
-
-	}
 	
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws IOException {
 		
-		Company c=new Company();
-		c.test1();
+	
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.redbus.in/");
+		driver.findElement(By.id("src")).sendKeys("chennai");
+		driver.findElement(By.id("dest")).sendKeys("banglore");
+		driver.findElement(By.xpath("//span[@class='dateText']")).click();
+//		TakesScreenshot screen=(TakesScreenshot) driver;
+//		File s = screen.getScreenshotAs(OutputType.FILE);
+//		File d=new File("C:\\Users\\lakshman\\Desktop\\gitclass\\ran.png");
+//		
+//		FileUtils.copyFile(s, d);
+		
+		
 		
 	}
 }
